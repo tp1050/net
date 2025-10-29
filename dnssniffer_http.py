8912924eb73b6e763090a858d1ef3e5aff76bd3a
tp1050 <tp1050@gmail.com>
2025-10-30 01:20:15 +0330

    aa

--- dnssniffer_http.py
+++ dnssniffer_http.py
@@ -5,17 +5,23 @@ from flask import Flask, request, make_response
 
 app = Flask(__name__)
 
-def build_reply(msg: bytes, fake_ip: str) -> bytes:
-    tx_id, flags, qdcount = struct.unpack('!HHH', msg[:6])
-    reply = bytearray(msg)
-    reply[2:4] = struct.pack('!H', 0x8180)          # QR=1, RD=1, RA=1
-    reply[6:8] = struct.pack('!H', 1)               # ANCOUNT = 1
-    answer = (
-        b'\xc0\x0c'                                 # name pointer
-        + struct.pack('!HHIH', 1, 1, 60, 4)        # TYPE A, CLASS IN, TTL, RDLEN
-        + socket.inet_aton(fake_ip)
-    )
-    return bytes(reply) + answer
+def build_reply(query: bytes, fake_ip: str) -> bytes:
+    tx_id, flags, qdcount = struct.unpack('!HHH', query[:6])
+
+    # copy header and fix flags / counts
+    header = bytearray(query[:12])
+    header[2:4] = struct.pack('!H', 0x8180)   # QR=1, RD=1, RA=1
+    header[6:8] = struct.pack('!H', 1)        # ANCOUNT = 1
+
+    # question section is everything up to the first zero-byte + 4 bytes (type+class)
+    end_q = 12
+    while query[end_q] != 0:
+        if query[end_q] & 0xC0:               # compression pointer
+            end_q += 2
+            break
+        end_q += 1 + query[end_q]
+    if query[end_q] == 0:
+        end_q +=r
 
 @app.route('/dns-query', methods=['GET', 'POST'])
 def dns_query():
